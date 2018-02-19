4fc29b6ef75b8a4006a7d9d781ee1afd3e8c6a7b
Ligia <ligia.venturott@gmail.com>
2018-02-19 08:26:31 -0300

    script returns a specified culumn from a csv file

--- Python/get_column_i_want-v2.py
+++ Python/get_column_i_want-v2.py
@@ -1,6 +1,7 @@
 import csv
 import unidecode,codecs
 import sys,tqdm
+delimiter = '|'
 try:
 	name_file_in = sys.argv[1]
 	name_file_out = name_file_in.replace('datasets','out_files')
@@ -15,15 +16,16 @@ except Exception as e:
 
 
 arquivo_sujo='tweets_positivos.csv'
-saida = codecs.open(name_file_in+'_CIW','w',)
-with codecs.open(name_file_in, 'r') as csvfile:
-	arquivo = csv.reader(csvfile, delimiter='|')
+saida = open(name_file_in+'_CIW','w',)
+with open(name_file_in, 'r') as arquivo:
+	# arquivo = csv.reader(csvfile, delimiter='|')
 	lines_read=0
-	for row in arquivo:
+	for linha in arquivo:
+		row = linha.split(delimiter)
 		lines_read+=1
-		# sys.stdout.flush()
-		# sys.stdout.write("Lines read: %d   \r" % (lines_read) )
-		print(lines_read)
+		sys.stdout.flush()
+		sys.stdout.write("Lines read: %d   \r" % (lines_read) )
+		# print(lines_read)
 		comment = row[column_i_want-1].replace('"','')
 		unaccented_string = unidecode.unidecode(comment)
 		comment = unaccented_string.lower()
